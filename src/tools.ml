82b844633424591a7cea3b859316c9770df359eb
Mat0s <49348079+Mat0s@users.noreply.github.com>
2023-12-26 12:24:21 +0100

    Create tools.ml

---
+++ src/tools.ml
@@ -0,0 +1,16 @@
+(* Yes, we have to repeat open Graph. *)
+open Graph
+
+(* assert false is of type ∀α.α, so the type-checker is happy. *)
+let clone_nodes gr = n_fold gr new_node empty_graph
+
+
+let gmap gr f = 
+  e_fold gr (fun acc arc -> new_arc acc {arc with lbl = f arc.lbl}) (clone_nodes gr)
+
+
+let add_arc g id1 id2 n = 
+  let update_label lbl = match lbl with l -> (l+n) in
+  match find_arc g id1 id2 with
+  | Some arc -> new_arc g {arc with lbl = update_label arc.lbl}
+  | None -> new_arc g {src = id1; tgt = id2; lbl = n}
